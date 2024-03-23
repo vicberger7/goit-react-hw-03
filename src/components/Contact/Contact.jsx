b7cf502db63e6a6e4fc9f78103958e67e7218103
Viktor Atsberher <143943277+vicberger7@users.noreply.github.com>
2024-03-23 21:38:58 +0100

    hw3-react-hlo

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,18 @@
 import css from "./Contact.module.css";
-// import { HiPhone, HiUser } from "react-icons";
+import { HiPhone, HiUser } from "react-icons/hi2";
 
 export default function Contact({ data: { id, name, number }, onDelete }) {
   return (
     <div className={css.container}>
       <div className={css.textContainer}>
-        {/* <HiPhone size={30} color="black" /> */}
-        <p className={css.text}>{name}</p>
-        {/* <HiUser size={30} color="black" /> */}
-        <p className={css.text}>{number}</p>
+        <div className={css.iconContainer}>
+          <HiPhone size={18} color="black" />
+          <p className={css.text}>{name}</p>
+        </div>
+        <div className={css.iconContainer}>
+          <HiUser size={18} color="black" />
+          <p className={css.text}>{number}</p>
+        </div>
       </div>
       <button className={css.btn} onClick={() => onDelete(id)}>
         Delete
